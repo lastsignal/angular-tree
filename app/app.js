58e5a03b44af5b3b88873c06c2ffdb920308f525
lastsignal <bahram@lastsignal.com>
2015-10-19 22:39:50 -0500

    major cleanup

--- app/app.js
+++ app/app.js
@@ -1,16 +1,16 @@
 (function () {
   'use strict';
 
-  angular.module('demoApp', ['ui.tree', 'ngRoute'])
+	angular.module('demoApp', ['ui.tree', 'ngRoute', 'datePicker'])
 
-    .config(['$routeProvider', function ($routeProvider) {
-      $routeProvider
-        .when('/basic', {
-          controller: 'BasicCtrl',
-          templateUrl: 'app/components/basic/basic-view.html'
-        })
-        .otherwise({
-          redirectTo: '/basic'
-        });
-    }]);
+	.config(['$routeProvider', function ($routeProvider) {
+	  $routeProvider
+		.when('/basic', {
+		  controller: 'BasicCtrl',
+		  templateUrl: 'app/components/basic/basic-view.html'
+		})
+		.otherwise({
+		  redirectTo: '/basic'
+		});
+	}]);
 })();
